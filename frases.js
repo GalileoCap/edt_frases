dc6de619e5c16b40e01f70b4da55b72fa2be1ab2
mauriciocap@gmail.com <galileocapp@gmail.com>
2019-02-12 15:25:50 -0300

    INIT frases.js

---
+++ frases.js
@@ -0,0 +1,12 @@
+frases= [
+	"Hello?",
+	"Hi, Jim from the frontend team here.",
+	"Hi Jim! How can I help you?", 
+	"I need to speak with Laura, is she there?",
+	"I'm afraid she's not here.",
+	"Do you want to leave a message?",
+	"Yes, we need more information about the cart feature, we are blocked.",
+	"I'm sending her a text message to her mobile so she knows you called.",
+	"Thanks, bye.",
+	"Bye.",
+];
