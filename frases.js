c500e7dd80f5603edfafe1597a74950c87df7ad6
GalileoCap <40899339+GalileoCap@users.noreply.github.com>
2019-02-14 09:24:33 -0300

    HIJOHN

--- frases.js
+++ frases.js
@@ -11,3 +11,17 @@ Dialogos.hijim= [
 	"Thanks, bye.",
 	"Bye.",
 ];
+Dialogos.hijohn= [
+	"Hello?",
+  	"Nicolas speaking",
+	"Hi Nicolas, can I speak with John?",
+	"Can I tell him who's calling?", 
+	"Yes, it's Mary Johnson",
+	"OK, I'm not sure if he's here. Please let me check",
+	"I'm afraid he's not here, he's in the sales department.",
+	"Could you please tell me the phone number of that deparment?",
+ 	"Right. I'll ask and send you an email. Could you please confirm?",
+	"Yes, sure. My email is mary@mybusiness.com",
+	"Thanks, bye.",
+	"Bye.",
+];
