eaa4f161aeea11e57ae33a2634728fad0adb6d5c
galileocapp@gmail.com <galileocapp@gmail.com>
2019-02-16 13:14:06 -0300

    FIX: "Nicolás" en inglés es "Nicholas"

--- frases.js
+++ frases.js
@@ -13,15 +13,15 @@ Dialogos.hijim= [
 ];
 Dialogos.hinick= [
 	"Mary: Hello?",
-  "Nicolas: Nicolas speaking",
-	"Mary: Hi Nicolas, can I speak with John?",
-	"Nicolas: Can I tell him who's calling?", 
+  "Nicholas: Nicolas speaking",
+	"Mary: Hi Nicholas, can I speak with John?",
+	"Nicholas: Can I tell him who's calling?", 
 	"Mary: Yes, it's Mary Johnson",
-	"Nicolas: OK, I'm not sure if he's here. Please let me check",
-	"Nicolas: I'm afraid he's not here, he's in the sales department.",
+	"Nicholas: OK, I'm not sure if he's here. Please let me check",
+	"Nicholas: I'm afraid he's not here, he's in the sales department.",
 	"Mary: Could you please tell me the phone number of that deparment?",
- 	"Nicolas: Right. I'll ask and send you an email. Could you please confirm?",
+ 	"Nicholas: Right. I'll ask and send you an email. Could you please confirm?",
 	"Mary: Yes, sure. My email is mary@mybusiness.com",
-	"Nicolas: Thanks, bye.",
+	"Nicholas: Thanks, bye.",
 	"Mary: Bye.",
 ];
